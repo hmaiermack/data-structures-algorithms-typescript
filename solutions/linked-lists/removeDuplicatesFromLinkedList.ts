f30d7ea447cecf987e75b7b3beec131783b1eece
Mack Maier <hmaiermack@gmail.com>
2021-08-23 15:52:56 -0700

    removeDuplicatesFromLinkedList solution

---
+++ solutions/linked-lists/removeDuplicatesFromLinkedList.ts
@@ -0,0 +1,33 @@
+//solution from algo expert
+
+export class LinkedList {
+    value: number
+    next: LinkedList | null
+
+    constructor(value: number) {
+        this.value = value
+        this.next = null
+    }
+}
+
+//linked list is always sorted in ascending order
+//assume you're always given at least one node
+function removeDuplicatesFromLinkedList(list: LinkedList): LinkedList {
+    //set current node to head of the linked list
+    let currentNode: LinkedList | null = list
+    while ( currentNode !== null ) {
+        let nextNode: LinkedList | null = currentNode.next
+        // if the value of the current node and the value of the next node are the same, 
+        // reassign the nextNode to its following nextNode and try the while statement again
+        while (nextNode !== null && nextNode.value === currentNode.value){
+            nextNode = nextNode.next
+        }
+
+        //if nextNode.value doesnt equal the currentNode.value then you can be sure there are no more duplicates 
+        //of currentNode.value and try the while statement on the next node
+        currentNode.next = nextNode
+        currentNode = nextNode
+    }
+
+    return list
+}
